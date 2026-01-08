3a9de71aa7671d57c318f514cd18391b39a6d63d
wertyg775 <danishshaifuddin.DL@gmail.com>
2026-01-08 22:09:24 +0800

    Changed backend logic and updated requirements.txt

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 # backend/app.py
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
-from transformers import AutoModelForSequenceClassification, AutoTokenizer
-from transformers import pipeline
-import greenwashing_analyzer
-import torch
 from greenwashing_analyzer import GreenwashingAnalyzer
+from fastapi.staticfiles import StaticFiles
+
+
 app = FastAPI()
+
 
 app.add_middleware(
     CORSMiddleware,
@@ -23,11 +25,14 @@ analyzer = GreenwashingAnalyzer(
 class AnalysisRequest(BaseModel):
     text: str
 
-@app.post("/api/analyze")
-async def analyze(request: AnalysisRequest):
-    return analyzer.analyze(request.text)
+@app.post("/api/analyze_additive")
+async def analyze_additive(request: AnalysisRequest):
+    return analyzer.analyze_additive(request.text)
 
 
 @app.get("/health")
 async def health():
     return {"status": "ok"}
+
+
+app.mount("/", StaticFiles(directory="frontend", html=True), name="frontend")
